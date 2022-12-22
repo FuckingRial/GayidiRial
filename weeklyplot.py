a0669ddc629b7b589c1a359317003ca1447b797d
Farbod <cppoxking@gmail.com>
2022-12-22 16:10:59 +0330

    Update weeklyplot.py

--- weeklyplot.py
+++ weeklyplot.py
@@ -1,6 +1,13 @@
 import requests
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 today = requests.get("https://bonbast-api.deta.dev/latest").json()
 yesterday = requests.get("https://bonbast-api.deta.dev/archive/").json()
-#for i in range(5):
+days = []
+days.append(today)
+days.append(yesterday)
+d = yesterday["date"].split("-")
+for i in range(5):
+  days.append(requests.get("https://bonbast-api.deta.dev/archive/"+d[0]+"-"+d[1]+str(int(d[2])-i-1)).json())
+print(days)
+  
