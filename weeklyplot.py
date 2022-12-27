1620f1e2c9812476acd3126f568e82991f13e755
Farbod <cppoxking@gmail.com>
2022-12-26 22:14:14 -0800

    Update weeklyplot.py

--- weeklyplot.py
+++ weeklyplot.py
@@ -1,6 +1,8 @@
 import requests
 from datetime import datetime as date
 import matplotlib.pyplot as plt
+from bidi.algorithm import get_display
+from arabic_reshaper import reshape
 
 weekdays_cte = ["دوشنبه","سه شنبه","چهارشنبه","پنج شنبه","جمعه","شنبه","یکشنبه"]
 
@@ -24,15 +26,16 @@ for i in range(6):
   usd.append(days[i+1]["usd"]["sell"])
 print(weekdays)
 print(usd)
-plt.plot(usd, weekdays)
+persian_weekdays = [get_display(reshape(label)) for label in weekdays]
+plt.plot(usd, persian_weekdays)
   
 # naming the x axis
-plt.xlabel('قیمت دلار')
+plt.xlabel(get_display(reshape('قیمت دلار'))
 # naming the y axis
-plt.ylabel('روز هفته')
+plt.ylabel(get_display(reshape('روز هفته'))
   
 # giving a title to my graph
-plt.title('پروژه گاییدن ریال')
+plt.title(get_display(reshape('پروژه گاییدن ریال'))
   
 # function to show the plot
 plt.savefig('foo.png')
