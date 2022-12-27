a57469630b7e61b9121578df2c84d4fb44838d20
Farbod <cppoxking@gmail.com>
2022-12-26 22:00:54 -0800

    Update weeklyplot.py

--- weeklyplot.py
+++ weeklyplot.py
@@ -1,5 +1,8 @@
 import requests
-#import matplotlib.pyplot as plt
+from datetime import datetime as date
+import matplotlib.pyplot as plt
+
+weekdays_cte = ["دوشنبه","سه شنبه","چهارشنبه","پنج شنبه","جمعه","شنبه","یکشنبه"]
 
 today = requests.get("https://bonbast-api.deta.dev/latest").json()
 yesterday = requests.get("https://bonbast-api.deta.dev/archive/").json()
@@ -11,3 +14,13 @@ for i in range(5):
   days.append(requests.get("https://bonbast-api.deta.dev/archive?date="+d[0]+"-"+d[1]+"-"+str(int(d[2])-i-1)).json())
 print(days)
   
+usd = []
+weekdays = []
+weekdays.append(weekdays_cte[date.today().weekday()])
+usd.append(days[0]["usd"]["sell"])
+for i in range(4):
+  dd = days[i+1]["date"].split("-")
+  weekdays.append(weekdays_cte[date(int(dd[0]),int(dd[1]),int(dd[2])).weekday()])
+  usd.append(days[i+1]["usd"]["sell"])
+print(weekdays)
+print(usd)
